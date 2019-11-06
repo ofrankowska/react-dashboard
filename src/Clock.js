cc4c5ed48431b04a89e00a251167a386a81e8fab
Ola <olafrankowska112@gmail.com>
2019-11-06 19:17:06 +0100

    addes tyles to Clock

--- src/Clock.js
+++ src/Clock.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+    time: {
+        color: "white",
+        fontSize: "8rem",
+        fontWeight: 400,
+        textShadow: "0 0 10px rgba(0, 0, 0, 0.568)"
+    }   
+}
 
 function Clock(props) {
-    const {hour, min, sec}= props;
+    const {hour, min, sec, classes}= props;
     function addZero(n){
         return (parseInt(n, 10) < 10 ? '0' : '') + n;
     }
     return (
-        <time>
+        <time className={classes.time}>
             {addZero(hour)}<span>:</span>{addZero(min)}<span>:</span>{addZero(sec)}
         </time>
     )
 }
 
-export default Clock;
+export default withStyles(styles)(Clock);
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
+import { withStyles } from "@material-ui/core/styles";
+import classNames from "classnames";
 
+import morningImg from './img/morning.jpg';
+import afternoonImg from './img/afternoon.jpg';
+import eveningImg from './img/evening.jpg';
+const styles = {
+    root: {
+        backgroundColor: "#1D2636",
+        backgroundSize: "cover",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center"
+    },
+    morning : {
+        backgroundImage: `url(${morningImg})`,
+
+    }, 
+    afternoon: {
+        backgroundImage: `url(${afternoonImg})`,
+    },
+    evening: {
+        backgroundImage: `url(${eveningImg})`,
+    }
+}
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -20,13 +45,17 @@ class Main extends Component {
     }
     render() {
         const { hour, min, sec } = this.state;
+        const {classes}=this.props;
         return (
-            <div>
+            <div className={classNames(classes.root, {
+                [classes.morning]: hour >= 6 || hour < 12,
+                [classes.afternoon]: hour >= 12 || hour < 18,
+                [classes.evening]: hour >= 18 || hour < 6,                
+            })}>
                 <Clock hour={hour} min={min} sec={sec} />
-
             </div>
         )
     }
 }
 
-export default Main;
+export default withStyles(styles)(Main);
