38c24d762e5288983e006ae3c45b20414b3c7e0c
Ola <olafrankowska112@gmail.com>
2019-12-12 20:22:27 +0100

    Updates

---
+++ precache-manifest.986bf27c376b532df3f13ceb42f330ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e497971a15bc14df721bc04d0d991bc",
+    "url": "/react-dashbaord/index.html"
+  },
+  {
+    "revision": "2fd91694689a493ce1b6",
+    "url": "/react-dashbaord/static/css/main.511356c3.chunk.css"
+  },
+  {
+    "revision": "e06de2f251426f21f8ad",
+    "url": "/react-dashbaord/static/js/2.e147d18d.chunk.js"
+  },
+  {
+    "revision": "72d118c4d2605ae51b18b50bc5fa4d7f",
+    "url": "/react-dashbaord/static/js/2.e147d18d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fd91694689a493ce1b6",
+    "url": "/react-dashbaord/static/js/main.7636cffb.chunk.js"
+  },
+  {
+    "revision": "4fae30a3a3cf4d8c7c84",
+    "url": "/react-dashbaord/static/js/runtime-main.a2baf6e8.js"
+  },
+  {
+    "revision": "59fcbcec54572daa173839b13c8c8f05",
+    "url": "/react-dashbaord/static/media/afternoon.59fcbcec.jpg"
+  },
+  {
+    "revision": "57ba52fb762413f5739cfcdb7cfb1cab",
+    "url": "/react-dashbaord/static/media/evening.57ba52fb.jpg"
+  },
+  {
+    "revision": "e9572e4269f639408e76a551d879953f",
+    "url": "/react-dashbaord/static/media/morning.e9572e42.jpg"
+  }
+]);
